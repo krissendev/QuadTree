4e07ec3d29dde240a21d5d27dc13e4a36b0505e7
krissendev <krissen@hotmail.no>
2025-03-10 15:58:21 +0100

    express migration

---
+++ app.js
@@ -0,0 +1,14 @@
+const path = require('path')
+
+
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 8080;
+
+app.use('/', express.static(path.join(__dirname, 'public')))
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port localhost:${port}/`)
+})
