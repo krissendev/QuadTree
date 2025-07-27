7889dfe5c4bf1ebabaa9cf3c4cb564ffea3740e3
krissendev <krissen@hotmail.no>
2025-07-27 12:56:23 +0200

    Migrate from prod branch. /public -> /static. app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 8080;
 
-app.use('/', express.static(path.join(__dirname, 'public')))
+app.use('/', express.static(path.join(__dirname, 'static')))
 
 
 app.listen(port, () => {
